69db443bb6a192a3214b8700783c6ec02e91b806
sumx86 <ichi256@abv.bg>
2020-06-20 12:13:09 +0300

    small fix

--- main.c
+++ main.c
@@ -17,14 +17,14 @@ int main( int argc, char **argv )
     head = vma_list;
 
     while ( vma_list != NULL ) {
-        if ( vma_list->perms & R && vma_list->perms & X ) {
-            printf( "%lu\n", vma_list->start );
-            printf( "%lu\n", vma_list->end );
-            printf( "%lu\n", vma_list->offset );
+        //if ( vma_list->perms & R && vma_list->perms & X ) {
+            printf( "%x%x\n", (uint32_t) ((vma_list->start >> 32) & 0xffffffff), (uint32_t) ((vma_list->start >> 0x0) & 0xffffffff) );
+            printf( "%x%x\n", (uint32_t) ((vma_list->end   >> 32) & 0xffffffff), (uint32_t) ((vma_list->end   >> 0x0) & 0xffffffff) );
+            printf( "%x\n",  vma_list->offset );
             printf( "%s\n",  vma_list->mmi );
-            printf( "%lu\n", vma_list->inode );
+            printf( "%x\n",  vma_list->inode );
             printf( "%s\n",  vma_list->image );
-        }
+        //}
         vma_list = vma_list->next;
     }
     pidmap__release( head );
